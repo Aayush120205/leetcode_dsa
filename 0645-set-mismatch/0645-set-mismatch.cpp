6776b3bf0d23489a14e58a8a3d140237d7dc39ea
AAYUSH ASWAL <aayushaswal3@gmail.com>
2026-07-14 12:19:12 +0530

    Time: 0 ms (100%), Space: 25.3 MB (62.25%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> hash(n + 1, 0);
+        long long n = nums.size();
+        long long SN = (n*(n+1))/2;
+        long long S2N = (n * (n+1) * (2*n+1))/6;
+        long long S=0,S2=0;
         for(int i=0;i<n;i++){
-            hash[nums[i]]++;
+            S+=nums[i];
+            S2+= (long long)nums[i] * (long long)nums[i];
         }
-        int repeating=-1,missing=-1;
-        for(int i=1;i<=n;i++){
-            if(hash[i]==2) repeating = i;
-            else if(hash[i]==0) missing = i;
-            if(repeating!= -1 && missing!= -1){
-                break;
-            }
-        }
-        return{repeating,missing};
+        long long val1=S-SN;
+        long long val2=S2-S2N;
+        val2=val2/val1;
+        long long x=(val1+val2)/2;
+        long long y= x-val1;
+        return{(int)x,(int)y} ;
     }
 };
